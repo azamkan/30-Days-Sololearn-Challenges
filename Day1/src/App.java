029826255d67f5765bf84beee3b9d67ac12fe9af
Abdullah Azam <abdullahazam640@gmail.com>
2024-03-11 17:05:45 +0800

    add: Day 1

---
+++ Day1/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int wins = 54;
+        int ties = 31;
+
+        double winspoin = wins * 1;
+        double tiespoin = ties * 0.5;
+        double poins = winspoin + tiespoin;
+        System.out.println(poins);
+    }
+}
